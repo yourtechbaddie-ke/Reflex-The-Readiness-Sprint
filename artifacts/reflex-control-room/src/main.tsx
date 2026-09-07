853e687710aba975805bca964c27ed49699f955c
Regina <muhatiaregina68@gmail.com>
2026-09-07 14:43:38 +0300

    style(control-room): load unified visual theme globally

--- artifacts/reflex-control-room/src/main.tsx
+++ artifacts/reflex-control-room/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css";
 import "./darker-approved-theme.css";
 import "./settings.css";
+import "./uniform-theme.css";
 import "./navigationEnhancer";
 
 createRoot(document.getElementById("root")!).render(
