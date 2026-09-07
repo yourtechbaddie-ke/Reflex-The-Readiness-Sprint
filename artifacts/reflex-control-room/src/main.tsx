54d97053f83607d58f12b2d7d9050352d3f7b572
Regina <muhatiaregina68@gmail.com>
2026-09-07 14:12:04 +0300

    feat(control-room): wire global home navigation

--- artifacts/reflex-control-room/src/main.tsx
+++ artifacts/reflex-control-room/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./darker-approved-theme.css";
+import "./navigationEnhancer";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
